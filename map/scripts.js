89b373679af215d00dd3df1599f1cff73eafaad3
drobotnik <ykabaza@gmail.com>
2015-11-03 23:38:16 +0000

    fixed basic

--- map/scripts.js
+++ map/scripts.js
@@ -1,1 +1,14 @@
+function initialize() {
+    var mapCanvas = document.getElementById('map');
+    var mapOptions = {
+        center: new google.maps.LatLng(0, 100),
+        zoom: 3,
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+    return new google.maps.Map(mapCanvas, mapOptions);
+};
 
+
+var map = initialize();
+
+map.data.loadGeoJson('data/test_geo.json');
